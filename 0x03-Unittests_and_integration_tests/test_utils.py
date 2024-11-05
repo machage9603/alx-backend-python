3a6a762557f7b3414957c2209a5dba37849382b0
machage9603 <mikemachage@gmail.com>
2024-11-05 04:41:59 +0300

    add solution for task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,52 +1,29 @@
 #!/usr/bin/env python3
 """
-This module has tests
+Test utils
 """
 
-from parameterized import parameterized
 import unittest
-
-
-def access_nested_map(nested_map, path):
-    """
-    This method tests parameterization
-    """
-    current = nested_map
-    for key in path:
-        current = current[key]
-    return current
+from parameterized import parameterized
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """
-    This class parameterize a unit test
-    """
+	"""_summary_
 
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
+	Args:
+		unittest (_type_): _description_
+	"""
 
-    def test_access_nested_map(self, nested_map, path, expected):
-        """
-        Tests access_nested_map with parameterized inputs
-        """
-        self.assertEqual(access_nested_map(nested_map, path), expected)
-
-    @parameterized.expand([
-        ({}, ("a")),
-        ({"a": 1}, ("a", "b")),
-    ])
-
-    def test_access_nested_map_exception(self, nested_map, path):
-        """
-        Tests access_nested_map raises KeyError with parameterized inputs
-        """
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), str(KeyError(path[-1])))
-
-
-if __name__ == "__main__":
-    unittest.main()
+	@parameterized.expand(
+		[
+			({"a": 1}, ("a",), 1),
+			({"a": {"b": 2}}, ("a",),{"b":2}),
+			({"a": {"b": 2}}, ("a", "b"), 2)
+		]
+	)
+	def test_access_nested_map(self, nested_map, path, expected_output):
+		"""_summary_
+		"""
+		result = access_nested_map(nested_map, path)
+		self.assertEqual(result, expected_output)
