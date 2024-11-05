80f5a5e2870f6f46315c1c7d2d14ecc816c5528d
machage9603 <mikemachage@gmail.com>
2024-11-05 05:02:00 +0300

    add solutions for tasks 4-8

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,7 +13,7 @@ class TestAccessNestedMap(unittest.TestCase):
     """_summary_
 
     Args:
-                                    unittest (_type_): _description_
+        unittest (_type_): _description_
     """
 
     @parameterized.expand(
@@ -35,7 +35,8 @@ class TestAccessNestedMap(unittest.TestCase):
             ({"a": 1}, ("a", "b"), KeyError)
         ]
     )
-    def test_access_nested_map_exception(self, nested_map, path, expected_output):
+    def test_access_nested_map_exception(self, nested_map, path,
+                                         expected_output):
         """_summary_
         """
         with self.assertRaises(expected_output) as context:
@@ -46,7 +47,7 @@ class TestGetJson(unittest.TestCase):
     """_summary_
 
     Args:
-            unittest (_type_): _description_
+                    unittest (_type_): _description_
     """
     @parameterized.expand(
         [
@@ -69,18 +70,35 @@ class TestMemoize(unittest.TestCase):
     """_summary_
 
     Args:
-            unittest (_type_): _description_
+                    unittest (_type_): _description_
     """
 
     def test_memoize(self):
+        """_summary_
+
+        Returns:
+                _type_: _description_
+        """
 
         class TestClass:
+            """_summary_
+            """
 
             def a_method(self):
+                """_summary_
+
+                Returns:
+                        _type_: _description_
+                """
                 return 42
 
             @memoize
             def a_property(self):
+                """_summary_
+
+                Returns:
+                        _type_: _description_
+                """
                 return self.a_method()
 
         test_obj = TestClass()
